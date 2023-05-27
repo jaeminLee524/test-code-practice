b833a2b06b8645b91e1e841310472ecf2ebee49e
jaeminLee524 <jaeminLee524@hanmail.net>
2023-05-27 15:07:17 +0900

    perform divide test case and boundary value analysis

--- src/main/java/sample/unit/CafeKiosk.java
+++ src/main/java/sample/unit/CafeKiosk.java
@@ -3,6 +3,7 @@ package sample.unit;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 import lombok.Getter;
 import sample.unit.beverage.Beverage;
 import sample.unit.order.Order;
@@ -13,6 +14,20 @@ public class CafeKiosk {
     private final List<Beverage> beverageList = new ArrayList<>();
     public void add(Beverage beverage) {
         beverageList.add(beverage);
+    }
+
+    public void add(Beverage beverage, int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("음료는 1잔 이상의 경우에만 주문하실 수 있습니다.");
+        }
+
+        for (int i = 0; i < count; i++) {
+            beverageList.add(beverage);
+        }
+    }
+
+    public void addAll(List<Beverage> beverages) {
+        beverageList.addAll(beverages);
     }
 
     public void remove(Beverage beverage) {
--- src/test/java/sample/unit/CafeKioskTest.java
+++ src/test/java/sample/unit/CafeKioskTest.java
@@ -1,7 +1,9 @@
 package sample.unit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import sample.unit.beverage.Americano;
 import sample.unit.beverage.Latte;
@@ -18,14 +20,34 @@ class CafeKioskTest {
     }
 
     @Test
+    void addSeveralBeverage() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        cafeKiosk.add(americano, 2);
+
+        assertThat(cafeKiosk.getBeverageList()).hasSize(2);
+        assertThat(cafeKiosk.getBeverageList().get(0).getName()).isEqualTo("아메리카노");
+        assertThat(cafeKiosk.getBeverageList().get(1).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void addZeroBeverage() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+
+        assertThatThrownBy(() -> cafeKiosk.add(new Americano(), 0))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("음료는 1잔 이상의 경우에만 주문하실 수 있습니다.");
+    }
+
+    @Test
     void add() {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.add(new Americano());
 
-        Assertions.assertThat(cafeKiosk.getBeverageList().size()).isEqualTo(1);
-        Assertions.assertThat(cafeKiosk.getBeverageList()).hasSize(1);
-        Assertions.assertThat(cafeKiosk.getBeverageList().get(0).getName()).isEqualTo("아메리카노");
-        Assertions.assertThat(cafeKiosk.getBeverageList().get(0).getPrice()).isEqualTo(4000);
+        assertThat(cafeKiosk.getBeverageList().size()).isEqualTo(1);
+        assertThat(cafeKiosk.getBeverageList()).hasSize(1);
+        assertThat(cafeKiosk.getBeverageList().get(0).getName()).isEqualTo("아메리카노");
+        assertThat(cafeKiosk.getBeverageList().get(0).getPrice()).isEqualTo(4000);
     }
 
     @Test
@@ -34,11 +56,11 @@ class CafeKioskTest {
         Americano americano = new Americano();
         cafeKiosk.add(americano);
 
-        Assertions.assertThat(cafeKiosk.getBeverageList()).hasSize(1);
+        assertThat(cafeKiosk.getBeverageList()).hasSize(1);
 
         cafeKiosk.remove(americano);
-        Assertions.assertThat(cafeKiosk.getBeverageList()).hasSize(0);
-        Assertions.assertThat(cafeKiosk.getBeverageList()).isEmpty();
+        assertThat(cafeKiosk.getBeverageList()).hasSize(0);
+        assertThat(cafeKiosk.getBeverageList()).isEmpty();
     }
 
     @Test
@@ -46,10 +68,10 @@ class CafeKioskTest {
         CafeKiosk cafeKiosk = new CafeKiosk();
         cafeKiosk.addAll(List.of(new Americano(), new Latte()));
 
-        Assertions.assertThat(cafeKiosk.getBeverageList()).hasSize(2);
+        assertThat(cafeKiosk.getBeverageList()).hasSize(2);
 
         cafeKiosk.clear();
-        Assertions.assertThat(cafeKiosk.getBeverageList()).hasSize(0);
-        Assertions.assertThat(cafeKiosk.getBeverageList()).isEmpty();
+        assertThat(cafeKiosk.getBeverageList()).hasSize(0);
+        assertThat(cafeKiosk.getBeverageList()).isEmpty();
     }
 }
