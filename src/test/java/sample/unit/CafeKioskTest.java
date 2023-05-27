b15901a1c530162a8b0b13a6e3e6084780595a62
이재민 <jaemin@MacBookPro.local>
2023-05-27 14:19:26 +0900

    perform a manual test

---
+++ src/test/java/sample/unit/CafeKioskTest.java
@@ -0,0 +1,18 @@
+package sample.unit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import sample.unit.beverage.Americano;
+
+class CafeKioskTest {
+
+    @Test
+    void add() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
+
+        System.out.println("음료가 담긴 총 개수: " + cafeKiosk.getBeverageList().size());
+        System.out.println("키오스크에 담긴 음료: " + cafeKiosk.getBeverageList().get(0).getName());
+    }
+}
