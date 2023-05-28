7eb462c7d997484dabce45dcafd5e989a3b3cc4b
jaeminLee524 <rkwoals524@hanmail.net>
2023-05-28 19:39:05 +0900

    moved file

---
+++ src/main/java/sample/cafekiosk/api/controller/order/OrderController.java
@@ -0,0 +1,25 @@
+package sample.cafekiosk.api.controller.order;
+
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.api.controller.order.request.OrderCreateRequest;
+import sample.cafekiosk.api.service.order.OrderService;
+
+@RequiredArgsConstructor
+@RestController
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/api/v1/orders/new")
+    public void createOrder(
+        @RequestBody OrderCreateRequest request
+    ) {
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        orderService.createOrder(request, registeredDateTime);
+    }
+}
