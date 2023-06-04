2231a019b7f9822bfa06728bea314399433f7667
jaeminLee524 <rkwoals524@hanmail.net>
2023-06-04 22:28:41 +0900

    practice BDDMockito

---
+++ src/test/java/sample/cafekiosk/api/service/mail/MailServiceTest.java
@@ -0,0 +1,59 @@
+package sample.cafekiosk.api.service.mail;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sample.cafekiosk.client.mail.MailSendClient;
+import sample.cafekiosk.domain.mail.MailSendHistory;
+import sample.cafekiosk.domain.mail.MailSendHistoryRepository;
+
+@ExtendWith(MockitoExtension.class)
+class MailServiceTest {
+
+    @Mock
+    private MailSendClient mailSendClient;
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+    @InjectMocks
+    private MailService mailService;
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail() {
+        // given
+
+        // stubbing
+//        when(mailService.sendMail(anyString(), anyString(), anyString(), anyString()))
+//            .thenReturn(true);
+
+        /*doReturn(true)
+            .when(mailSendClient)
+            .sendMail(anyString(), anyString(), anyString(), anyString());*/
+
+        //given
+        BDDMockito.given(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
+            .willReturn(true);
+
+        // when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        // then
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+
+        assertThat(result).isTrue();
+    }
+
+}
